6e64b0b69ed747bf1557a570c7e716a23e69fcdf
Lavie92 <paintjinx1@gmail.com>
2023-08-08 08:03:15 +0700

    alarm with head up notification

--- app/src/main/java/com/example/myapplication/receiver/DismissReceiver.java
+++ app/src/main/java/com/example/myapplication/receiver/DismissReceiver.java
@@ -1,7 +1,9 @@
 package com.example.myapplication.receiver;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
 import androidx.core.app.NotificationManagerCompat;
 
 import com.example.myapplication.woker.ConstantsManager;
@@ -10,10 +12,8 @@ public class DismissReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Xử lý khi người dùng nhấn vào nút "Tắt"
-        // Đóng thông báo
-        NotificationManagerCompat notificationManager =
-                NotificationManagerCompat.from(context);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(
+                context.getApplicationContext());
         notificationManager.cancel(ConstantsManager.NOTIFICATION_ID);
     }
 }
--- app/src/main/java/com/example/myapplication/woker/AlarmWorker.java
+++ app/src/main/java/com/example/myapplication/woker/AlarmWorker.java
@@ -62,7 +62,6 @@ public class AlarmWorker extends Worker {
             if (ringtone != null) {
                 ringtone.play();
                 showNotification(noteContent);
-//            createNotification();
                 try {
                     Thread.sleep(10000);
                 } catch (InterruptedException e) {
@@ -71,50 +70,7 @@ public class AlarmWorker extends Worker {
 
                 ringtone.stop();
             }
-//            // Tạo PendingIntent cho khi người dùng nhấn vào thông báo
-//            Intent intent = new Intent(context, MainActivity.class);
-//            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-//            stackBuilder.addNextIntentWithParentStack(intent);
-//            PendingIntent pendingIntent =
-//                    stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//            // Tạo action cho nút "Mở"
-//            Intent openIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.example.com"));
-//            PendingIntent openPendingIntent = PendingIntent.getActivity(context, 0, openIntent,
-//                    PendingIntent.FLAG_IMMUTABLE);
-//
-//            // Tạo action cho nút "Tắt"
-//            Intent dismissIntent = new Intent(context, DismissReceiver.class);
-//            PendingIntent dismissPendingIntent =
-//                    PendingIntent.getBroadcast(context, 0, dismissIntent, PendingIntent.FLAG_IMMUTABLE);
-//
-//            // Tạo notification
-//            NotificationCompat.Builder builder =
-//                    new NotificationCompat.Builder(context, ConstantsManager.CHANNEL_ID)
-//                            .setSmallIcon(R.mipmap.ic_launcher)
-//                            .setContentTitle("Thông báo báo thức")
-//                            .setContentText(noteContent)
-//                            .setPriority(NotificationCompat.PRIORITY_HIGH)
-//                            .setContentIntent(pendingIntent)
-//                            .addAction(R.mipmap.ic_launcher, "Mở", openPendingIntent)
-//                            .addAction(R.drawable.circle, "Tắt", dismissPendingIntent)
-//                            .setDefaults(Notification.DEFAULT_ALL)
-//                            .setAutoCancel(true);
-//
-//            // Hiển thị notification
-//            NotificationManagerCompat notificationManager =
-//                    NotificationManagerCompat.from(context);
-//            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) !=
-//                    PackageManager.PERMISSION_GRANTED) {
-//                requestNotificationPolicyPermission();
-//                return Result.failure();
-//            }
-//            notificationManager.notify(ConstantsManager.NOTIFICATION_ID, builder.build());
         }
-        // Lấy dữ liệu từ InputData
-
-
-
         return Result.success();
     }
 
@@ -122,55 +78,41 @@ public class AlarmWorker extends Worker {
         if (content != null) {
             int newlineIndex = content.indexOf("\n");
             if (newlineIndex != -1) {
-                content = content.substring(0, newlineIndex); //
+                content = content.substring(0, newlineIndex);
             }
             if (content.length() > 20) {
                 content = content.substring(0, 20) + "...";
             }
         }
-        // Tạo notification
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(getApplicationContext(), ConstantsManager.CHANNEL_ID)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle(content)
-                        .setContentText(content)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent,
+                PendingIntent.FLAG_IMMUTABLE);
+        Intent dismissIntent = new Intent(context, DismissReceiver.class);
+        PendingIntent dismissPendingIntent = PendingIntent.getBroadcast(context, 0, dismissIntent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, ConstantsManager.CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(content)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
 
-        // Hiển thị notification
-        NotificationManagerCompat notificationManager =
-                NotificationManagerCompat.from(getApplicationContext());
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) !=
                 PackageManager.PERMISSION_GRANTED) {
             requestNotificationPolicyPermission();
             return;
         }
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(ConstantsManager.NOTIFICATION_ID, builder.build());
     }
+
     private void requestNotificationPolicyPermission() {
-        // Xin cấp quyền ACCESS_NOTIFICATION_POLICY tại đây
         Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
-    private void createNotification() {
-        // Tạo PendingIntent cho khi người dùng nhấn vào thông báo
-        Intent intent = new Intent(context, MainActivity.class); // Thay YourActivity bằng Activity bạn muốn mở khi nhấn vào thông báo
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // Xây dựng thông báo
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, ConstantsManager.CHANNEL_ID)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Thông báo báo thức")
-                .setContentText("Nhấn để tắt báo thức")
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true); // Tự động đóng thông báo khi người dùng nhấn vào
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) !=
-                PackageManager.PERMISSION_GRANTED) {
-            requestNotificationPolicyPermission();
-            return;
-        }
-        // Hiển thị thông báo
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(ConstantsManager.NOTIFICATION_ID, builder.build());
+    private PendingIntent getCancelNotificationIntent() {
+        Intent cancelIntent = new Intent(context, DismissReceiver.class);
+        return PendingIntent.getBroadcast(context, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
